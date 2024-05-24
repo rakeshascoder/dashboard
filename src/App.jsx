f72b9c6cfeddfd0d5654b54baee8cd16ead8847f
rakeshascoder <rakeshsheoran1202@gmail.com>
2024-05-24 08:51:11 +0530

    dashboard start

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import Asidebar from "./Components/Asidebar";
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      <Asidebar />
+    </div>
+  );
+};
+
+export default App;
